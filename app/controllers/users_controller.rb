7aa23b6f47f873a47577bf4e36b6a2862d0c3660
dmitrii-silver <dmitrii.serebriakov@hotmail.com>
2021-12-20 11:37:07 +0000

    users + sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,78 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :create, :new]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+  
+  def index
+    @users = User.all
+  end
 
-	def show
-		@hello = "Привет, МИР!"
-	end
+
+
+  def new
+    # Если юзер залогинен, отправляем его на главную с сообщением
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
+    # Иначе, создаем болванку нового пользователя.
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
+  end
+
+
+
+  def edit
+  end
+
+  def show
+    # берём вопросы у найденного юзера
+    @questions = @user.questions.order(created_at: :desc)
+
+    # Для формы нового вопроса создаём заготовку, вызывая build у результата вызова метода @user.questions.
+    @new_question = @user.questions.build    
+
+    @questions_amount = @questions.count
+    @answers_amount = @questions.count(&:answer)
+    @unanswered_amount = @questions_amount - @answers_amount
+  end
+
+  def update
+    # Получаем параметры нового (обновленного) пользователя с помощью метода user_params
+    # пытаемся обновить юзера
+    if @user.update(user_params)
+      # Если получилось, отправляем пользователя на его страницу с сообщением
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      # Если не получилось, как и в create, рисуем страницу редактирования
+      # пользователя со списком ошибок
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    # берём объект params, потребуем у него иметь ключ
+    # :user, у него с помощью метода permit разрешаем
+    # набор инпутов. Ничего лишнего, кроме них, в пользователя не попадёт
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                               :name, :username, :avatar_url)
+  end
+
+  def load_user
+    # защищаем от повторной инициализации с помощью ||=
+    @user ||= User.find params[:id]
+  end
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+	
+	def user_avatar(user)
+      if user.avatar_url.present?
+        user.avatar_url
+      else
+        asset_path 'avatar.jpg'
+    end
+  end
+
+  def inclination(number, vopros, voprosa, voprosov)
+    result = number % 10 unless (11..14).include?(number % 100)
+      case result
+      when 1 then vopros
+      when 2..4 then voprosa
+      else voprosov
+      end
+  end
 end
+ 
